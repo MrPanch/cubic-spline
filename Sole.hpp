64a3aff3ed5195ad76731bd49fc7bb8ea7525fd4
Mr_Panch <gleb.panchenko.2013@gmail.com>
2020-05-16 14:56:11 +0300

    прогонка как метод класса Slope

--- Sole.hpp
+++ Sole.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 
-template <size_t width = 3, size_t height = 3>
+template <size_t width, size_t height>
 class Sole {
     public:
         Sole();
@@ -13,11 +13,15 @@ class Sole {
         std::vector<std::vector<double>>& GetMatrix() { return matrix_;  };
         std::vector<double>& GetFreeVariables() {return free_variables_; };
 
-        double& GetA(int i) { return matrix_[i+1][i]; };
-        double& GetB(int i) { return matrix_[i][i];   };
-        double& GetC(int i) { return matrix_[i][i+1]; };
+        double& GetA(int i)  { return matrix_[i+1][i]; };
+        double& GetB(int i)  { return matrix_[i][i];   };
+        double& GetC(int i)  { return matrix_[i][i+1]; };
+
+
         template <size_t m, size_t n>
         friend std::istream &operator>>(std::istream& stream, Sole<m, n>& sole);
+
+        std::vector<double> SeqThomas();
     private:
         std::vector<std::vector<double>> matrix_;
         std::vector<double> free_variables_; // Столбец свободных коэффициентов
--- main.cpp
+++ main.cpp
@@ -6,39 +6,11 @@
 // Потому что разрешение темплейтов, подробней https://www.codeproject.com/Articles/48575/How-to-Define-a-Template-Class-in-a-h-File-and-Imp
 
 
-int main() {
-    Sole<5, 5> system;
+int main(int argc, char** argv) {
+//    const size_t size = 30;
+    Sole<3, 3> system;
     std::cin >> system;
-    std::vector<double> gamma(system.GetHeight(), 0);
-    std::vector<double> alpha(system.GetHeight() - 1, 0);
-    std::vector<double> beta(system.GetHeight(), 0);
-    std::vector<double> x(system.GetHeight(), 0);
-
-    gamma[0] =   system.GetMatrix()[0][0];           //нулевые коэффициенты
-    alpha[0] = - system.GetMatrix()[0][1] / gamma[0];
-    beta[0]  =   system.GetFreeVariables()[0] / gamma[0];
-
-    for(int i = 1; i < system.GetHeight(); ++i) {  // от 1 до N
-        if ( i == system.GetHeight() - 1){ // конкретно для N-го случая
-            gamma[i] = system.GetB(i) + system.GetA(i -1) * alpha[i - 1];
-            beta[i] = (system.GetFreeVariables()[i] - system.GetA(i-1) * beta[i - 1]) / gamma[i];
-            break;
-        }
-        gamma[i] = system.GetB(i) + system.GetA(i -1) * alpha[i - 1];
-        alpha[i] = -system.GetC(i) / gamma[i];
-        beta[i] = (system.GetFreeVariables()[i] - system.GetA(i-1) * beta[i - 1]) / gamma[i];
-
-    }
-
-    x[system.GetWidth() - 1] = beta[system.GetWidth() - 1];
-    for(size_t i = system.GetWidth() - 2; i > 0; --i) { //size_t не может становится равной нулю
-        x[i] = alpha[i] * x[i + 1] + beta[i];
-    }
-    x[0] = alpha[0] * x[1] + beta[0]; // считаем отдельно для нулевого икса
-
-    std::cout << "The solution of the Sole is:" << std::endl;
-    for(int i = 0; i < system.GetWidth(); i++) {
-        std::cout << x[i] << "   ";
-    }
+    auto x = system.SeqThomas();
+    return 0;
 
 }
